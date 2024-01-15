d56f5b3a07c4a1eb4ffb0ee610236e21e57b1f61
Mahafuz Ahmed <mahafuzaadeez5@gmail.com>
2024-01-16 03:41:53 +0530

    some modification

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
+const TMDB_TOKEN = process.env.TMDB_BEARER_TOKEN;
+
 export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png"
 
 export const API_CONSTANTS = {
     method: 'GET',
     headers: {
       accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmODVhODk4MTdkY2Q0MTI0YzI3NTEyZmVkZWVmNWQyYyIsInN1YiI6IjYwZWQ3OWM3MTQwYmFkMDA0NjI0ODczMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OT9x16zOHhvDLibxhcEU81MZ7Erk6YbK47ofyP2mUh0'
+      Authorization: `Bearer ${TMDB_TOKEN}`
     }
   };
 
